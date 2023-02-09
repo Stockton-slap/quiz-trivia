375a430417cd135bce184d263c9cbd5608dcbbc7
Andrey Yurchenko <spree92spree92@gmail.com>
2023-02-09 12:56:58 +0200

    add deployment changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="quiz-trivia">
       <Provider store={store}>
         <Theme>
           <App />
