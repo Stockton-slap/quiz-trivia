e430e6d2a23d8364e3bdd5a082800a9043c7e4cb
Andrey Yurchenko <spree92spree92@gmail.com>
2023-02-09 13:52:11 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import App from "./components/App";
 // import { BrowserRouter } from "react-router-dom";
 import Theme from "./components/Theme/Theme";
 import { Provider } from "react-redux";
-import store from "./redux/store";
 import { HashRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Theme>
-          <App />
-        </Theme>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Theme>
+            <App />
+          </Theme>
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
